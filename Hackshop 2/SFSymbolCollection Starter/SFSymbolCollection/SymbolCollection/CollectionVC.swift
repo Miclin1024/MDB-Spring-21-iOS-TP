ec9ec7b4f444305c336a18379860b55e94c5e45d
Michael Lin <miclin@berkeley.edu>
2021-02-22 18:55:10 -0800

    Added starter

---
+++ Hackshop 2/SFSymbolCollection Starter/SFSymbolCollection/SymbolCollection/CollectionVC.swift
@@ -0,0 +1,18 @@
+//
+//  CollectionVC.swift
+//  SFSymbolCollection
+//
+//  Created by Michael Lin on 2/22/21.
+//
+
+import UIKit
+
+class CollectionVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
+
+
